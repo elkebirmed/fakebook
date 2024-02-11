0b7f550d09d47af844657feec230b7e2f1571559
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2024-02-11 11:25:29 +0100

    Add `Livewire` & `Volt`

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Fakebook' }}</title>
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
